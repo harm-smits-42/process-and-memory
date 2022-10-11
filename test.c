a2a67aa9f727b8bcc1b1cf9a3302ab725dc6ec4e
Harm Smits <harmsmitsdev@gmail.com>
2022-10-11 10:41:15 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,16 +1,22 @@
 #include <sys/syscall.h>
 //#include <linux/pid_info.h>
-#include "/usr/src/linux-5.16.9/include/uapi/linux/pid_info.h"
+#include "/usr/src/linux-5.19.2/include/uapi/linux/pid_info.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 		return 2;
 	struct pid_info in;
-	syscall(450, &in, atoi(argv[1]));
+	int res = syscall(451, &in, atoi(argv[1]));
+
+	if (res) {
+		perror("getpidinfo");
+		return 1;
+	}
 
 	printf("pid=%d\n", in.pid);
 	printf("state=%d\n", in.state);
